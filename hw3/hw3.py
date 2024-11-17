877e6463db9896893afd8d1b2791c5f44b33c059
Joe Braha <jbraha@nyu.edu>
2024-11-17 13:52:36 -0500

    double ranking added and bm25 logic moved out to query processor
    not fully tested, but I think it works

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,3 +1,4 @@
+from io import StringIO
 import h5py
 import ranx
 import faiss
@@ -26,6 +27,14 @@ def load_h5_embeddings(file_path, id_key="id", embedding_key="embedding"):
     return ids, embeddings
 
 
+def get_subset_h5(ids, embeddings, id_subset: set):
+    filtered = embeddings[(ids in id_subset)]
+    fids = ids[(ids in id_subset)]
+    print(len(filtered))
+    print(fids[:5], filtered[:5])
+    return fids, filtered
+
+
 # might need to adjust these appropriately based on your file directory structure
 dir = "MSMARCO-Embeddings/"
 file_path = dir + "msmarco_passages_embeddings_subset.h5"
@@ -38,9 +47,8 @@ k = 10  # number of nearest neighbors to fetch for each query
 
 
 # calculates HNSW rankings on all queries in queries_path file, and return indicies
-def calculate_hnsw(queries_path: str, index):
-    ids, query_embeddings = load_h5_embeddings(queries_path)
-    D, I = index.search(query_embeddings, k)
+def calculate_hnsw(ids, embeddings, index):
+    D, I = index.search(embeddings, k)
     print(ids[:5], I[:5])
     return I
 
@@ -50,87 +58,28 @@ def evaluate(results):
     ...
 
 
-def call_bm25(
-    queries: list[tuple[int, str]], program_name, num_results
-) -> list[tuple[int, list[int]]]:
-    """calls the BM25 index from program_name on each of queries and returns
-    a list of tuples with num_results docids per query"""
-
-    # for every query:
-    # call the bm25 processor with a query
-    # put the resulting docids in a list
-    # return the list
-
-    print("running queries...")
-    qtype = "d"
-    proc = subprocess.Popen(
-        program_name, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True
-    )
-    fullqstring = f"{num_results}\n"
-    for query in queries:
-        # out = proc.communicate(f"{num_results}\n{qtype}\n{query[1]}".encode())
-        fullqstring += f"{qtype}\n{query[1]}"
-    out = proc.communicate(fullqstring)
-    if out[1]:
-        raise Exception(out[1])
-    out = out[0]
-
-    # loop to parse the results from proc while keeping track of which query we're up to
-    query_results = []
-    current_query = 0
-    started_new_query = False
-    current_results = []
-    for line in out:
-        if line[0] <= "9":  # then it's a result, not text
-            words = line.split()
-            print(words)
-            current_results.append((int(words[2][:-1])))
-        elif started_new_query:
-            started_new_query = False
-            print(current_results)
-            query_results.append((queries[current_query][0], current_results))
-            current_results = []
-            current_query += 1
-
-    # iterout = out.splitlines()[5:]
-    # for i in range(num_results):
-    #     if i > len(iterout) - 1:
-    #         break
-    #     line = iterout[i]
-    #     if not line:
-    #         break
-    #     words = line.split()
-    #     # print(words)
-    #     # query_results.append((int(word[2][:-1]), float(word[4])))
-    #     query_results.append((int(words[2][:-1])))
-
-    return query_results
-
-
-def get_embeddings_from_docids(docids):
+def get_embeddings_from_docids(docids, dids, dembeddings):
     # creates a HNSW index on only the given docids
-    ...
+    # d = dids[(dids in docids)]
+    d = dids[np.isin(dids, docids)]
+    e = dembeddings[np.isin(dids, docids)]
+    # e = dembeddings[(dids in docids)]
+    index = build_index(d, e)
+    return index
 
 
-def rerank(queries, docids, index):  # TODO:
-    query_embeddings = ...  # get embeddings from queries
-    doc_embeddings = get_embeddings_from_docids(docids)
-    D, I = index.search(query_embeddings, k)
-    return I
-
-
-def double_rank(queries, index):
+def double_rank(qids, qembeddings, queries: dict):
     # open the file and find the query strings
-    initial_results = call_bm25(
-        queries, qproc, 20
-    )  # TODO: fine tune number of results to get
-    print(initial_results)
-    result = rerank(queries, initial_results, index)
-    return result
+    ret = {}
+    for query, docids in queries.items():
+        index = get_embeddings_from_docids(docids, dids, dembeddings)
+        qembedded = qembeddings[(qids == query)]
+        D, I = index.search(qembedded, k)
+        ret[query] = I
+    return ret
 
 
-def build_index() -> faiss.IndexIDMap:
-    ids, embeddings = load_h5_embeddings(file_path)
+def build_index(ids, embeddings) -> faiss.IndexIDMap:
     dim = len(embeddings[0])
     print(f"Dimension: {dim}")
     index = faiss.IndexIDMap(faiss.IndexHNSWFlat(dim, M))
@@ -150,26 +99,32 @@ def get_query_strings(query_strings):
     return strings
 
 
-def get_all_query_strings():
-    print("Loading query strings...")
-    strings = (
-        get_query_strings(query_strings_dir + "queries.dev.tsv")
-        + get_query_strings(query_strings_dir + "queries.train.tsv")
-        + get_query_strings(query_strings_dir + "queries.eval.tsv")
-    )
-    return strings
+# reads file of results formatted as \n-sep "<qid> <result 1> <result 2> etc"
+def read_results(file: str):
+    results = {}  # dict of qid -> list of results
+    with open(file) as f:
+        for line in f:
+            words = line.split()
+            results[int(words[0])] = [int(a) for a in words[1:]]
+    return results
 
 
 if __name__ == "__main__":
-    bm25_rank = call_bm25(get_all_query_strings(), qproc, 10)
-    print(bm25_rank)
+    # before doing this: run bm25 on 10 results to 1 file, and do it on 100ish results to another file
+    # bm25_rank = read_results("bm25_rank")
 
-    index = build_index()
-    print(index)
+    qids, qembeddings = load_h5_embeddings(queries_path)
+    dids, dembeddings = load_h5_embeddings(file_path)
+    print(qids[:5], qembeddings[:5])
 
-    vector_rank = calculate_hnsw(queries_path, index)
-    double_rank = double_rank("", index)
+    # HNSW run
+    # index = build_index(dids, dembeddings)
+    # print(index)
+    # vector_rank = calculate_hnsw(qids, qembeddings, index)
 
-    evaluate(bm25_rank)
+    bm25_big = read_results("bm25_big")
+    double_rank = double_rank(qids, qembeddings, bm25_big)
+
+    # evaluate(bm25_rank)
     evaluate(vector_rank)
     evaluate(double_rank)
