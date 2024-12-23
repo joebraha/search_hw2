d58de3089660f1a376117a70a1a5d4e05a04c110
Sonali Seshadri <sonalitara@cs.uchicago.edu>
2024-12-23 18:54:13 +0100

    modified for formatting, will update every 100 docs.

--- project/local_ollama.py
+++ project/local_ollama.py
@@ -18,16 +18,19 @@ client = Client(
 #     ],
 # )
 
-# msg_content = "Look at this passage of text and formulate 5 queries that could be given to a search engine about this passage. Format the response so it is one string of words, no numbering or line breaks. Passage: "
-# msg_content = "Rephrase this passage of text using as many new words as is possible and only output the generated content, no notes or introductions needed, no line breaks: "
-msg_content = "Come up with 5 queries that a user might submit to a search engine looking for this passage- use as much new phrasing as possible. Only return the queries. Passage: "
+# doc expansion
+# msg_content = "Rephrase this passage of text using as many new words as is possible and only output the generated content, no notes or introductions needed, no line breaks, ignore the number at the beginning: "
+
+# query expansion
+msg_content = "Come up with 5 queries that a user might submit to a search engine looking for this passage- use as much new phrasing as possible. Only return the queries, ignore the number at the beginning, include no extra text as introduction. Passage: "
 
 # take in 10 documents from 10_documents.txt
-with open('10_documents.txt', 'r') as file:
+with open('random_filtered_docs.txt', 'r') as file:
     documents = file.readlines()
 
-with open('10_documents_modified.txt', 'w') as file:
+with open('10k_docs_query_expansion.txt', 'w') as file:
     start_time = time.time()  # Start the timer
+    doc_counter = 0
     for doc in documents:
         stripped_doc = doc.strip()
         doc_msg = msg_content + stripped_doc
@@ -42,9 +45,13 @@ with open('10_documents_modified.txt', 'w') as file:
         )
         doc_queries = response.message.content
         doc_queries_stripped = doc_queries.replace('\n', ' ')
-        modified_document = stripped_doc + " " + doc_queries_stripped + "\n"
-        file.write(modified_document)
+        doc_queries_stripped += "\n"
+        # modified_document = stripped_doc + " " + doc_queries_stripped + "\n"
+        file.write(doc_queries_stripped)
+        doc_counter += 1
+        if (doc_counter % 100 == 0):
+            print(f"Processed {doc_counter} documents in {time.time() - start_time:.2f} seconds")
 
     end_time = time.time()  # End the timer
     elapsed_time = end_time - start_time
-    print(f"Modified documents have been written to 10_documents_modified.txt in {elapsed_time:.2f} seconds")
+    print(f"results have been written to file in {elapsed_time:.2f} seconds")
